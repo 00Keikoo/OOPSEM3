54752158d1977f077cd3a8252d0416b4d8980ad2
andikazafka <andikazafka04@gmail.com>
2025-11-05 09:53:00 +0000

    Ini di laptop bagas

---
+++ bab19/try/PersonalComputer.java
@@ -0,0 +1,9 @@
+public class PersonalComputer{
+	public MotherBoard motherboard;
+	public os OperatingSystem;
+	public mouse Mouse;
+
+	public PersonalComputer(){
+		this.motherBoard = new MotherBoard();
+	}
+}
